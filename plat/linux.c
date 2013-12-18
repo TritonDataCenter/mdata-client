3aeb907915ea2106b48de980ece0374e73220674
Joshua M. Clulow <jmc@joyent.com>
2013-12-18 21:24:23 +0000

    clean up linux platform-specific code

--- plat/linux.c
+++ plat/linux.c
@@ -32,94 +32,6 @@ typedef struct mdata_plat {
 	int mpl_conn;
 } mdata_plat_t;
 
-static int
-raw_mode(int fd, char **errmsg)
-{
-	struct termios tios;
-
-	if (tcgetattr(fd, &tios) == -1) {
-		*errmsg = "could not set raw mode on serial device";
-		return (-1);
-	}
-
-	tios.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-	tios.c_oflag &= ~(OPOST);
-	tios.c_cflag |= (CS8);
-	tios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-
-	/*
-	 * As described in "Case C: MIN = 0, TIME > 0" of termio(7I), this
-	 * configuration will block waiting for at least one character, or
-	 * the expiry of a 100 millisecond timeout:
-	 */
-	tios.c_cc[VMIN] = 0;
-	tios.c_cc[VTIME] = 1;
-
-	if (tcsetattr(fd, TCSAFLUSH, &tios) == -1) {
-		*errmsg = "could not get attributes from serial device";
-		return (-1);
-	}
-
-	return (0);
-}
-
-static int
-open_md(int *outfd, char **errmsg, int *permfail)
-{
-	int fd;
-	char scrap[100];
-	ssize_t sz;
-	struct flock l;
-
-	if ((fd = open(SERIAL_DEVICE, O_RDWR | O_EXCL |
-	    O_NOCTTY)) == -1) {
-		*errmsg = "Could not open serial device.";
-		if (errno != EAGAIN && errno != EBUSY && errno != EINTR)
-			*permfail = 1;
-		return (-1);
-	}
-
-	/*
-	 * Lock the serial port for exclusive access:
-	 */
-	l.l_type = F_WRLCK;
-	l.l_whence = SEEK_SET;
-	l.l_start = l.l_len = 0;
-	if (fcntl(fd, F_SETLKW, &l) == -1) {
-		fprintf(stderr, "could not lock: %s\n", strerror(errno));
-		return (-1);
-	}
-
-	/*
-	 * Set raw mode on the serial port:
-	 */
-	if (raw_mode(fd, errmsg) == -1) {
-		(void) close(fd);
-		*permfail = 1;
-		return (-1);
-	}
-
-	/*
-	 * Because this is a shared serial line, we may be part way through
-	 * a response from the remote peer.  Read (and discard) data until we
-	 * cannot do so anymore:
-	 */
-	do {
-		sz = read(fd, &scrap, sizeof (scrap));
-
-		if (sz == -1 && errno != EAGAIN) {
-			*errmsg = "Failed to flush serial port before use.";
-			(void) close(fd);
-			return (-1);
-		}
-
-	} while (sz > 0);
-
-	*outfd = fd;
-
-	return (0);
-}
-
 
 int
 plat_send(mdata_plat_t *mpl, string_t *data)
@@ -155,17 +67,13 @@ plat_recv(mdata_plat_t *mpl, string_t *data, int timeout_ms)
 			char buf[2];
 			ssize_t sz;
 
-			sz = read(mpl->mpl_conn, buf, 1);
-			if (sz == 0) {
-				fprintf(stderr, "WHAT, NO DATA?!\n");
-				continue;
-			}
-
-			if (buf[0] == '\n') {
-				return (0);
-			} else {
-				buf[1] = '\0';
-				dynstr_append(data, buf);
+			if ((sz = read(mpl->mpl_conn, buf, 1)) > 0) {
+				if (buf[0] == '\n') {
+					return (0);
+				} else {
+					buf[1] = '\0';
+					dynstr_append(data, buf);
+				}
 			}
 		}
 		if (event.events & EPOLLERR) {
@@ -182,7 +90,7 @@ plat_recv(mdata_plat_t *mpl, string_t *data, int timeout_ms)
 }
 
 void
-plat_fini(mdata_plat_t *mpl __UNUSED)
+plat_fini(mdata_plat_t *mpl)
 {
 	if (mpl != NULL) {
 		if (mpl->mpl_epoll != -1)
@@ -243,7 +151,8 @@ plat_init(mdata_plat_t **mplout, char **errmsg, int *permfail)
 		goto bail;
 	}
 
-	if (open_md(&mpl->mpl_conn, errmsg, permfail) != 0) {
+	if (unix_open_serial(SERIAL_DEVICE, &mpl->mpl_conn, errmsg,
+	    permfail) != 0) {
 		goto bail;
 	}
 
